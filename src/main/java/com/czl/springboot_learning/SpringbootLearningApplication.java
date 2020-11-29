f17ec27ba840c841b7ba4d3a6ab1382ee7f540ef
chenzl <953282353@qq.com>
2020-11-30 00:13:12 +0800

    学习springboot

---
+++ src/main/java/com/czl/springboot_learning/SpringbootLearningApplication.java
@@ -0,0 +1,13 @@
+package com.czl.springboot_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czl/springboot_learning/SpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czl.springboot_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
